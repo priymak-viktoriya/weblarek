bc9404d3b1ccaab619cc260cb6440ab0eb7e3e25
priymak-viktoriya <priymak.viktoriya@yandex.ru>
2026-09-06 08:48:57 +0300

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,32 @@ export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
+export type TPayment = 'card' | 'cash';
+
+// Интерфейс товара (с сервера)
+export interface IProduct {
+  id: string;
+  description: string;
+  image: string;
+  title: string;
+  category: string;
+  price: number | null;
+}
+
+// Интерфейс данных покупателя (для заказа)
+export interface IBuyer {
+  payment: TPayment;
+  email: string;
+  phone: string;
+  address: string;
+}
+
+export interface IOrderRequest extends IBuyer {
+    total: number;
+    items: string[];
+};
+
+export interface IOrderResultApi {
+    items: IProduct[];
+    total: number;
+}
