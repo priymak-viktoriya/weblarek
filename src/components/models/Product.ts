ee6387708f8cb9b82fc02d88461275764f867348
priymak-viktoriya <priymak.viktoriya@yandex.ru>
2026-09-07 17:13:45 +0300

    Product.ts

---
+++ src/components/models/Product.ts
@@ -0,0 +1,33 @@
+import { IProduct } from '../../types/';
+
+export class Products {
+    private items: IProduct[] = [];
+    private selectedItem: IProduct | null = null;
+
+    constructor() {}
+
+    setItems(items: IProduct[]): void {
+        this.items = items;
+    }
+
+    getItems(): IProduct[] {
+        return this.items;
+    }
+
+    getItem(id: string): IProduct | undefined {
+        return this.items.find(item => item.id === id);
+    }
+
+    setSelectedItem(item: IProduct): void {
+        this.selectedItem = item;
+    }
+
+    getSelectedItem(): IProduct | null {
+        return this.selectedItem;
+    }
+}
+
+    getProduct(): IProduct | null {
+        return this.selectedProduct;
+    }
+}
