e66d468ae46109d0ce78ef53e06db54b3224eb20
priymak-viktoriya <priymak.viktoriya@yandex.ru>
2026-09-14 11:04:59 +0300

    Basket.ts

--- src/components/models/Basket.ts
+++ src/components/models/Basket.ts
@@ -1,7 +1,17 @@
 import { IProduct } from '../../types';
+import { IEvents } from '../base/Events';
 
 export class Basket {
     private items: IProduct[] = [];
+    private eventBus: IEvents;
+
+    constructor(events: IEvents) {
+        this.eventBus = events;
+    }
+
+    private notify(): void {
+        this.eventBus.emit('basket:changed', { items: this.items });
+    }
 
     getItems(): IProduct[] {
         return this.items;
@@ -10,15 +20,23 @@ export class Basket {
     addItem(item: IProduct): void {
         if (!this.hasItem(item.id)) {
             this.items.push(item);
+            this.notify();
         }
     }
 
     removeItem(id: string): void {
+        const before = this.items.length;
         this.items = this.items.filter(item => item.id !== id);
+        if (before !== this.items.length) {
+            this.notify();
+        }
     }
 
     clear(): void {
-        this.items = [];
+        if (this.items.length) {
+            this.items = [];
+            this.notify();
+        }
     }
 
     getTotal(): number {
